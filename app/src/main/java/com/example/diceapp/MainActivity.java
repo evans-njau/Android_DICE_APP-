c408184093e48f8154ba426c4e4b79f1097716c2
evans-njau <evansn873@gmail.com>
2024-06-12 05:29:18 +0300

    My dice app in Java

---
+++ app/src/main/java/com/example/diceapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.diceapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageViewDice;
+    private final Random rng = new Random();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+        imageViewDice = findViewById(R.id.image_dice);
+        imageViewDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RollDice();
+            }
+        });
+    }
+    private void RollDice(){
+        int randomNumber = rng.nextInt(6) + 1;
+        switch (randomNumber){
+            case 1:
+                imageViewDice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageViewDice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageViewDice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageViewDice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageViewDice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageViewDice.setImageResource(R.drawable.dice6);
+        }
+    }
+}
